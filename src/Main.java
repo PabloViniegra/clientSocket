ff0ff8184abc1f0a04d653397743818fb6979137
PabloViniegra <pablovpmadrid@gamil.com>
2021-01-21 19:14:13 +0100

    ejercicio2 - cliente

--- src/Main.java
+++ src/Main.java
@@ -1,30 +1,21 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.PrintWriter;
+import java.io.*;
 import java.net.Socket;
 import java.net.UnknownHostException;
 
 public class Main {
     public static void main(String[] args) {
         try (Socket client = new Socket("localhost", 8080)) {
-            PrintWriter out = new PrintWriter(client.getOutputStream(),true);
-            BufferedReader in = new BufferedReader(new InputStreamReader(client.getInputStream()));
-            BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-            String cadena = "",eco = "";
-            System.out.println("Introduzca un texto: ");
-            do {
-                cadena = reader.readLine();
-                out.println(cadena);
-                eco = in.readLine();
-                System.out.println("ECO RECIBIDO DEL SERVIDOR "+ eco);
-                System.out.println("Introduzca un texto");
-                cadena = reader.readLine();
-            } while (!cadena.equals("*"));
-            out.close();
-            in.close();
-            System.out.println("FINALIZADA LA CONEXIÓN");
-            reader.close();
+            byte[] buffer = new byte[4096];
+            InputStream is = client.getInputStream();
+
+            FileOutputStream fos = new FileOutputStream("aux_recibido.txt");
+            int count;
+            while ((count = is.read(buffer)) > 0) {
+                System.out.println(count);
+                fos.write(buffer, 0, count);
+            }
+            is.close();
+            fos.close();
         } catch (UnknownHostException e) {
             e.printStackTrace();
         } catch (IOException e) {
