e6a74140c8b28799330d698ac62c4b7a9f002358
PabloViniegra <pablovpmadrid@gamil.com>
2021-02-04 20:27:12 +0100

    multiclient

--- src/Main.java
+++ src/Main.java
@@ -3,32 +3,31 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.net.Socket;
-import java.net.UnknownHostException;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        try (Socket client = new Socket("localhost", 8080)) {
-            PrintWriter out = new PrintWriter(client.getOutputStream(),true);
-            BufferedReader in = new BufferedReader(new InputStreamReader(client.getInputStream()));
-            BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-            String cadena = "",eco = "";
-            System.out.println("Introduzca un texto: ");
+        Scanner sc = new Scanner(System.in);
+        try {
+            Socket socket = new Socket("localhost",8080);
+            BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+            PrintWriter out = new PrintWriter(socket.getOutputStream(),true);
+
+            String texto;
+            String received;
             do {
-                cadena = reader.readLine();
-                out.println(cadena);
-                eco = in.readLine();
-                System.out.println("ECO RECIBIDO DEL SERVIDOR "+ eco);
-                System.out.println("Introduzca un texto");
-                cadena = reader.readLine();
-            } while (!cadena.equals("*"));
-            out.close();
+                System.out.print("Escribe:");
+                texto = sc.nextLine();
+                out.println(texto);
+                received = in.readLine();
+                System.out.println("Texto recibido: " + received);
+            } while (!texto.equals("#"));
             in.close();
-            System.out.println("FINALIZADA LA CONEXIÓN");
-            reader.close();
-        } catch (UnknownHostException e) {
-            e.printStackTrace();
+            out.close();
+            socket.close();
         } catch (IOException e) {
             e.printStackTrace();
         }
+
     }
 }
