5dc00312adff20f765c39c2d8ad2b698cf151ef6
PabloViniegra <pablovpmadrid@gamil.com>
2021-02-04 13:13:14 +0100

    acabado

--- src/Main.java
+++ src/Main.java
@@ -4,24 +4,24 @@ import java.net.UnknownHostException;
 import java.util.concurrent.TimeUnit;
 
 public class Main {
-    public static void main(String[] args) {
-        Thread thread = null;
-        Socket client = null;
-        BufferedReader bufferedReader = null;
-        try {
-            client = new Socket("localhost", 8080);
-            bufferedReader = new BufferedReader(new InputStreamReader(client.getInputStream()));
-            Message message = new Message("", client);
-            thread = new Thread(message);
-            thread.start();
-            String line;
-            while ((line = bufferedReader.readLine()) != null) {
-                System.out.println(line);
-            }
-        } catch (UnknownHostException e) {
-            e.printStackTrace();
-        } catch (IOException e) {
-            e.printStackTrace();
+    public static void main(String[] args) throws IOException {
+        String sentence;
+        String modifiedSentence;
+        Socket clientSocket;
+        while(true){
+            BufferedReader inFromUser = new BufferedReader(new InputStreamReader(System.in));
+
+            clientSocket = new Socket("localhost", 2343);
+
+            DataOutputStream out = new DataOutputStream(clientSocket.getOutputStream());
+            BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+
+            System.out.println("Ready");
+            sentence = in.readLine();
+            out.writeBytes(sentence + '\n');
+            modifiedSentence = in.readLine();
+            System.out.println(modifiedSentence);
         }
+
     }
 }
